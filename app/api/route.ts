6ed6ce1876f3228bb7163fd01956c7eab8f88190
Patryk Omiotek <patryk@macbook.local>
2023-05-29 16:28:04 +0200

    feat: get and post method

--- app/api/route.ts
+++ app/api/route.ts
@@ -3,11 +3,14 @@ import { NextResponse, NextRequest } from 'next/server';
 
 export async function GET(request: NextRequest) {
     // console.log('Request: ', request);
-    return NextResponse.json({ status: 'ok' })
+    return NextResponse.json({ status: 'ok1' })
 }
 
-export async function POST(request: Request) {
+export async function POST(request: NextRequest) {
     const responseData = await request.json();
     console.log(responseData);
-    return NextResponse.json(responseData);
+    return NextResponse.json({
+        message: "response from server",
+        ...responseData
+    });
 }
