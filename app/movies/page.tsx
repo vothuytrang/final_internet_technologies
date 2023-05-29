b3718adbb06a4cd5f43b40ed8be255794570129a
Patryk Omiotek <patryk@macbook.local>
2023-05-29 17:13:17 +0200

    feat: component with useState

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,24 +1,31 @@
+import { useState } from 'react';
 import { Header } from '../../components/Header';
 
-// export default function Contact() {
-function Movies() {
+const mockMovies = [{
+    id: 1,
+    title: 'Title one',
+    category_name: 'Category 1'
+}, {
+    id: 2,
+    title: 'Title two',
+    category_name: 'Category 2'
+}];
 
-    const movies = [{
-        id: 1,
-        title: 'Title one',
-        category_name: 'Category 1'
-    }, {
-        id: 2,
-        title: 'Title two',
-        category_name: 'Category 2'
-    }];
+interface Movie {
+    id: string;
+    title: string;
+    category_name: string;
+}
+
+function Movies() {
+    const [movies, setMovies] = useState<Movie[] | null>(null);
 
     return (
         <main className="mt-6">
             <Header>Movies</Header>
             <p className="mt-5">Movies list</p>
             <div>
-                {movies.map((elem) => {
+                {movies && movies.map((elem) => {
                     return (
                         <div key={elem.id}>{elem.title} ({elem.category_name})</div>
                     )
